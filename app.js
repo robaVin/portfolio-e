6d52d31f54dce746c30b38f52e3faf987f472026
robaVin <64101828+robaVin@users.noreply.github.com>
2025-07-30 02:06:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/pictures', express.static('public/pictures'));
 
 // Homepage = Gallery
 app.get('/', (req, res) => {
